46cb3c69a37b795687b6fc2ea9e6cad893dfd308
AleRyp <andrik.koz@gmail.com>
2020-11-10 16:14:12 +0200

    UPD. Added update, get user profile

--- src/profiles/serializers.py
+++ src/profiles/serializers.py
@@ -7,5 +7,31 @@ class GetUserNetSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = UserNet
-        exclude = ("password", "last_login", "is_active", "is_staff", "is_superuser")
+        exclude = (
+            "password",
+            "last_login",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions"
+        )
+
+
+class GetUserNetPublicSerializer(serializers.ModelSerializer):
+    """Вивід публічного інфо про user
+    """
+    class Meta:
+        model = UserNet
+        exclude = (
+            "email",
+            "phone",
+            "password",
+            "last_login",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions"
+        )
 
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,19 +1,19 @@
-from rest_framework.generics import RetrieveAPIView, UpdateAPIView
+from rest_framework.viewsets import  ModelViewSet
 from rest_framework import permissions
 
 from .models import UserNet
-from .serializers import GetUserNetSerializer
+from .serializers import GetUserNetSerializer, GetUserNetPublicSerializer
 
 
-class GetUserNetView(RetrieveAPIView):
-    """Вивід профіля user
+class UserNetPublicView(ModelViewSet):
+    """Вивід публічного профіля user
     """
     queryset = UserNet.objects.all()
-    serializer_class = GetUserNetSerializer
+    serializer_class = GetUserNetPublicSerializer
+    permission_classes = [permissions.AllowAny]
 
-
-class UpdateUserNetView(UpdateAPIView):
-    """Редагування користувача
+class UserNetView(ModelViewSet):
+    """Вивід профіля user
     """
     serializer_class = GetUserNetSerializer
     permission_classes = [permissions.IsAuthenticated]
